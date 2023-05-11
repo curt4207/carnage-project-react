fdf02cbbdb6b620f860237abe1c85df3b5d9dad6
curtis clayton <curtisclayton4270@gmail.com>
2023-05-10 23:58:01 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+// import * as THREE from 'three';
+
+import hoverEffect from 'hover-effect'
+
+function Carnage(params) {
+  new hoverEffect({
+   parent: document.querySelector(".distortion"),
+   intensity: 0.4,
+   image1: "./src/assets/Carnage-kneeling.png",
+   image2: "./src/assets/Carnage-standing.png",
+   displacementImage: "./src/assets/fogDiss.png",
+ });
+  
+}
+
+function App() {
+useEffect(() => {
+Carnage()
+}, [Carnage])
+  return (
+    <>
+    <div className="landing">
+        <div className="intro">
+            <h1>Carnage</h1>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui dolor exercitationem voluptatum? Sit eius voluptatem magni odio sapiente, possimus, doloribus praesentium, consequatur dolores quas obcaecati qui aspernatur maiores hic mollitia.
+            </p>
+        </div>
+      
+        <div className="distortion"></div>
+    
+    </div>
+    </>
+  )
+}
+
+export default App
