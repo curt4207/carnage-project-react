003233059a5d04e74194cbdc07efeeb252a18343
curtis clayton <curtisclayton4270@gmail.com>
2023-05-13 03:38:46 -0400

    project 95% finshed

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,51 @@
-import { useState, useEffect, useRef } from 'react'
-import './App.css'
-import hoverEffect from 'hover-effect'
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+import hoverEffect from "hover-effect";
+
 
 function App() {
-
   const container = useRef();
 
-useEffect(() => {
-  new hoverEffect({
-    parent: container.current,
-    intensity: 0.4,
-    image1:  "/CarnageKneeling.png",
-    image2: "/CarnageStanding.png",
-    displacementImage: "/fogDiss.png",
-  });
-},[container] )
-
+  useEffect(() => {
+    new hoverEffect({
+      parent: container.current,
+      intensity: 0.4,
+      image1: "/CarnageKneeling.png",
+      image2: "/CarnageStanding.png",
+      displacementImage: "/Carnage_Main.jpg",
+    });
+  }, [container]);
 
   return (
     <>
-    <div className="landing">
+      <div className="landing">
         <div className="intro">
-            <h1>Carnage</h1>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui dolor exercitationem voluptatum? Sit eius voluptatem magni odio sapiente, possimus, doloribus praesentium, consequatur dolores quas obcaecati qui aspernatur maiores hic mollitia.
-            </p>
+          <h1>Carnage</h1>
+          <p>
+            I was hollowed out inside... empty... just like my other. We were
+            both sick... because we were incomplete... but not anymore. We
+            healed each other... made each other whole. Neither one of us can
+            survive without the other. Forget host. Forget symbiote. There is
+            only Carnage.
+          </p>
+          <h5>— Carnage</h5>
         </div>
-      
+
         <div
-        className="parent"
-        id="imgContainer"
-        ref={container}
-        style={{
-          width: 600,
-          height: 600,
+          className="parent"
+          id="imgContainer"
+          ref={container}
+          style={{
+            width: 650,
+            height: 650,
+            overflow: "hidden",
+          }}
+        />
+      </div>
+        <h6>by Curtis Clayton</h6>
         
-        }}
-      />
-    
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
